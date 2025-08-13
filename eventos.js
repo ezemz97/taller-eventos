753598ca514f5e8fa990c8f8b7a52ba87c414743
ezemz97 <eze.mz@outlook.com>
2025-08-12 22:12:02 -0300

    Arreglar propagacion de eventos

--- eventos.js
+++ eventos.js
@@ -1,2 +1,8 @@
 let divBoton = document.getElementsByClassName('contenedor__boton')[0];
 divBoton.addEventListener('click', () => alert('Hola! Soy el div'));
+
+let btn = document.getElementById('btnSaludar');
+btn.addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert('Hola!');
+});
